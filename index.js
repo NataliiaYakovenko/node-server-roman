feed49a8ba8c670370bd1b06e4325f47b5351a88
Nataliia Yakovenko <yakovenkonatali999@gmail.com>
2025-05-25 09:06:26 +0300

    Add common js module

--- index.js
+++ index.js
@@ -1,2 +1,3 @@
-const MyMath = require("./utils/MyMath")
-console.log(MyMath)
+const { MyMath, object, superFunction } = require("./utils/MyMath");
+console.log(MyMath.sum(5,5));
+console.log(superFunction())
--- utils/MyMath.js
+++ utils/MyMath.js
@@ -4,6 +4,20 @@ class MyMath {
     static multy = (a, b) => a * b;
     static div = (a, b) => a / b;
   }
+
+  const object = {
+    key1: 'value1',
+    key2: 'value2',
+  }
+
+  const superFunction = ()=>{
+    return 1+2
+  }
   
-  module.exports = MyMath;
+  //експортуємо 1 об'єкт і більше нічого
+  //module.exports = MyMath;
   
+  //експортуємо всі об'єкти в файлі.js
+  module.exports = {
+    MyMath,object,superFunction
+  };
