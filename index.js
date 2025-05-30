c2b49829004ac718cfbd6c82c4c19112a52db807
Nataliia Yakovenko <yakovenkonatali999@gmail.com>
2025-05-30 20:23:25 +0300

    Add middleware

--- index.js
+++ index.js
@@ -4,13 +4,28 @@ const app = express();
 
 const PORT = 5000;
 
-app.get("/", (request, response) => {
-  //  '/' - http://localhost:5000
-  response.send("Hello world");
-});
+app.get(
+  "/",
+  (request, response,next) => {                //  '/' - http://localhost:5000
+    // response.send("Hello world");
+    console.log("first requestHandler");
+
+    request.newfield = 'super-important-value'
+
+    next()
+  },
+  (request, response,next) => {
+    console.log("seconf requestHandler");
+    next()
+  },
+  (request, response,next) => {
+    console.log("third requestHandler");
+    console.log(request.newfield)
+  }
+);
 
 app.get("/index.html", (request, response) => {
-  response.status(404).send('Text index.html');
+  response.status(404).send("Text index.html");
 });
 
 app.listen(PORT, () => {
